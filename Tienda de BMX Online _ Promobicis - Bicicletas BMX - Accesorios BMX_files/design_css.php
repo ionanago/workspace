b026263aaf74e280ec0bf527cc7519b94f4949d0
ionanago <51776373+ionanago@users.noreply.github.com>
2019-06-27 10:57:12 +0200

    Add files via upload

---
+++ Tienda de BMX Online _ Promobicis - Bicicletas BMX - Accesorios BMX_files/design_css.php
@@ -0,0 +1,29 @@
+
+.col_left_design {
+	padding-top:0px;
+	padding-left:0px;
+	padding-right:3px;
+	padding-bottom:10px;
+}
+.col_right_design {
+	padding-top:0px;
+	padding-left:3px;
+	padding-right:0px;
+	padding-bottom:10px;
+}
+.botonera_design {
+	background-image:url('images/design/botonera.jpg');
+	width:932px;
+	height:27px;
+}
+.botonera_pie_design {
+	background-image:url('images/design/pie_pagina.png');
+	width:968px;
+	height:70px;
+}
+.separador_central {
+	padding-top:0px;
+	padding-left:16px;
+	padding-right:0px;
+	padding-bottom:0px;
+}
